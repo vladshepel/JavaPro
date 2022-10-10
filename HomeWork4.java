89de15f7127174d78a1a3b88d4114b39857bff0b
Vlad <110630463+vladshepel@users.noreply.github.com>
2022-10-10 15:37:23 +0300

    Add files via upload

---
+++ HomeWork4.java
@@ -0,0 +1,28 @@
+package HomeWork;
+
+import java.util.Scanner;
+
+public class HomeWork4 {
+
+    public static void main(String[] Args) {
+
+        int warli = 13;
+        int arcli = 24;
+        int ridli = 46;
+        int allLi = 860*(warli+arcli+ridli);
+
+        System.out.println(" total attack of the clan Li " + allLi);
+
+        int warmin = 9;
+        int arcmin = 35;
+        int ridmin = 12;
+        double allmin = (int) 860*1.5*(9+35+12);
+
+        System.out.println(" total attack of the clan Min " + allmin);
+
+
+
+
+
+    }
+}
